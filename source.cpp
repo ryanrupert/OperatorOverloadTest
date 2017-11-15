f8972fc4787713e993341f9352221d99d54e6722
Ryan Rupert <mudge25+code@gmail.com>
2017-11-15 11:33:10 -0800

    Add source.cpp file
    Made main function
    Add class test with all needed members

---
+++ source.cpp
@@ -0,0 +1,27 @@
+/**
+* Created by Ryan Rupert
+* Date: 2017-11-15
+* Source: None
+* This is a test of how operator overloading
+*/
+
+#include <iostream>
+#include <map>
+
+class Test
+{
+	public:
+		//Source: http://www.cplusplus.com/forum/beginner/64186/
+		friend std::istream& operator >> (std::istream & in, Test& i);
+		friend std::ostream& operator << (std::ostream & out, Test& o);
+	private:
+		enum Status { TEST, TEST2, TEST3, TEST4 };
+		Status status;
+		//Source: https://stackoverflow.com/a/10514661
+		std::map<std::string, Test::Status> m = { {"Test", TEST}, {"Test2", TEST2}, {"Test3", TEST3}, {"Test4", TEST4} };
+};
+
+int main()
+{
+	return 0;
+}
